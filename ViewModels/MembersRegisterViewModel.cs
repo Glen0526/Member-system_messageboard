94ebbd403b2f37efc4e06c1a3f4b12bcdf6f5643
Glen0526 <88875161+Glen0526@users.noreply.github.com>
2021-08-15 11:42:54 +0800

    Add files via upload

---
+++ ViewModels/MembersRegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    //註冊用ViewModel
+    public class MembersRegisterViewModel
+    {
+        public Members newMember { get; set; }
+        [DisplayName("密碼")]
+        [Required(ErrorMessage ="請輸入密碼")]
+        public string Password { get; set; }
+
+        [DisplayName("確認密碼")]
+        [Compare("Password",ErrorMessage="兩次密碼輸入不一致")]
+        [Required(ErrorMessage="請輸入正確密碼")]
+        public string PasswordCheck { get; set; }
+    }
+}
